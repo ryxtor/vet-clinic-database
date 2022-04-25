1269bc7194f4aa643c2c131e9e01a327ad767b68
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-04-25 18:15:46 -0300

    Fix schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
-    date_of_birth DATA,
+    date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
