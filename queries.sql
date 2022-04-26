fdcce7b3eb37ccb82dea308d1db660eaf50e1e37
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-04-26 18:40:58 -0300

    Change * to species

--- queries.sql
+++ queries.sql
@@ -18,9 +18,9 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
-SELECT * FROM animals;
+SELECT species FROM animals;
 ROLLBACK;
-SELECT * FROM animals;
+SELECT species FROM animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
